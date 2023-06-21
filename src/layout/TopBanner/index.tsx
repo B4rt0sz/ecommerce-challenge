ead6c1975705c2500f9611533a27917c619fffea
B4rt0sz <bart8.developer@gmail.com>
2023-06-22 01:05:19 +0200

    add TopBanner component

--- src/layout/TopBanner/index.tsx
+++ src/layout/TopBanner/index.tsx
@@ -1,5 +1,16 @@
+import './index.scss'
+import { useIsDesktopDevice } from 'hooks/useMediaQuery'
+
+const bannerText = 'Tylko dzisiaj! Darmowa dostawa dla zamówień od 100zł'
+
 const TopBanner = () => {
-  return <div className='topBanner'>topBanner</div>
+  const isDesktop = useIsDesktopDevice()
+
+  return (
+    <div className={`topBanner ${isDesktop ? 'topBanner__desktop' : 'topBanner__mobile'}`}>
+      <p className={isDesktop ? 'topBanner__textDesktop' : 'topBanner__textMobile'}>{bannerText}</p>
+    </div>
+  )
 }
 
 export default TopBanner
