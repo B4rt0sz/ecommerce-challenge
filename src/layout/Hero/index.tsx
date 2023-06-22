5e001beadb06b414844f6571eccac373d33af3a3
B4rt0sz <bart8.developer@gmail.com>
2023-06-22 18:30:20 +0200

    add Hero component

--- src/layout/Hero/index.tsx
+++ src/layout/Hero/index.tsx
@@ -1,5 +1,15 @@
+import './index.scss'
+import Logo3Icon from 'components/icons/Logo3Icon'
+
 const Hero = () => {
-  return <div className='hero'>hero</div>
+  return (
+    <div className='hero'>
+      <div className='hero__container'>
+        <p>Code&design: Webjaksklepu.eu</p>
+        <Logo3Icon />
+      </div>
+    </div>
+  )
 }
 
 export default Hero
