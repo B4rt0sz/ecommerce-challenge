c885d8efa8e57b5211ee4a9d46dd5de0656fe635
B4rt0sz <bart8.developer@gmail.com>
2023-06-23 18:04:45 +0200

    add Highlights component

--- src/layout/Highlights/index.tsx
+++ src/layout/Highlights/index.tsx
@@ -1,5 +1,28 @@
+import './index.scss'
+import ArrowIcon from 'components/icons/ArrowIcon'
+import CupIcon from 'components/icons/CupIcon'
+import PersonIcon from 'components/icons/PersonIcon'
+import PathIcon from 'components/icons/PathIcon'
+
+const highlightsList = [
+  { icon: <CupIcon />, title: 'Najszybsza dostawa' },
+  { icon: <ArrowIcon />, title: 'Aż 180 dni na zwrot' },
+  { icon: <PersonIcon />, title: 'Profesjonalne doradztwo' },
+  { icon: <PathIcon />, title: 'Darmowa dostawa od 200 zł' },
+]
+
 const Highlights = () => {
-  return <div className='highlights'>highlights</div>
+  return (
+    <div className='highlights'>
+      <div className='highlights__container'>
+        {highlightsList.map((item) => (
+          <div className='highlights__container-item' key={item.title}>
+            {item.icon} <p>{item.title}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default Highlights
