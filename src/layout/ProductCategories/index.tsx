d57c5ab85eb6ea4eafb394ed2c8357ab145e6ee7
B4rt0sz <bart8.developer@gmail.com>
2023-06-23 19:18:13 +0200

    add ProductCategories component

--- src/layout/ProductCategories/index.tsx
+++ src/layout/ProductCategories/index.tsx
@@ -1,5 +1,25 @@
+import './index.scss'
+import categoryImg1 from './images/category1.png'
+import categoryImg2 from './images/category2.png'
+import categoryImg3 from './images/category3.png'
+
+const categoryImgList = [categoryImg1, categoryImg2, categoryImg3]
+
 const ProductCategories = () => {
-  return <div className='productCategories'>productCategories</div>
+  return (
+    <div className='productCategories'>
+      <div className='productCategories__images'>
+        {categoryImgList.map((img, index) => (
+          <img key={index} src={img} alt='category-img' />
+        ))}
+      </div>
+      <div className='productCategories__choose'>
+        <p>Lub wybierz coś specjalnego</p>
+        <button>Kawa</button>
+        <button>Tabletki czyszczące do ekspresu</button>
+      </div>
+    </div>
+  )
 }
 
 export default ProductCategories
