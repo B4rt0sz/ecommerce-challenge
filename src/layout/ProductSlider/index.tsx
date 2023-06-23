0ba3179f9a9b8ab55b4d1924e046f7468a9cc7cf
B4rt0sz <bart8.developer@gmail.com>
2023-06-23 19:00:24 +0200

    add ProductSlider component

--- src/layout/ProductSlider/index.tsx
+++ src/layout/ProductSlider/index.tsx
@@ -1,5 +1,27 @@
+import './index.scss'
+import { useIsDesktopDevice } from 'hooks/useMediaQuery'
+import SimpleImageSlider from 'react-simple-image-slider'
+import promoDesktop from './images/promoDesktop.png'
+import promoMobile from './images/promoMobile.png'
+
+const desktopPromoList = [promoDesktop, promoDesktop, promoDesktop]
+const mobilePromoList = [promoMobile, promoMobile, promoMobile]
+
 const ProductSlider = () => {
-  return <div className='productSlider'>productSlider</div>
+  const isDesktop = useIsDesktopDevice()
+
+  return (
+    <div className='productSlider'>
+      <SimpleImageSlider
+        width={isDesktop ? 870 : 341}
+        height={isDesktop ? 409 : 434}
+        images={isDesktop ? desktopPromoList : mobilePromoList}
+        showBullets={true}
+        showNavs={false}
+        autoPlay={true}
+      />
+    </div>
+  )
 }
 
 export default ProductSlider
