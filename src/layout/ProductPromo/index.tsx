02a2384d8ca5edf792a339aef9d39f2247962acb
B4rt0sz <bart8.developer@gmail.com>
2023-06-23 19:57:32 +0200

    add ProductPromo component

--- src/layout/ProductPromo/index.tsx
+++ src/layout/ProductPromo/index.tsx
@@ -1,5 +1,65 @@
+import './index.scss'
+import { useIsDesktopDevice } from 'hooks/useMediaQuery'
+import { Navigation } from 'swiper'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
+import 'swiper/css/navigation'
+import productImg1 from './images/product1.png'
+import productImg2 from './images/product2.png'
+import productImg3 from './images/product3.png'
+import productImg4 from './images/product4.png'
+
+const productList = [
+  {
+    img: productImg1,
+    title: 'Tabletki czyszczące do ekspresuSiemens Bosch TZ80001 2w1 - oryginał',
+    newPrice: '35,40zł',
+    oldPrice: '38,90zł',
+  },
+  {
+    img: productImg2,
+    title: '3x Filtr Seltino Ocea zamiennik do Philips Saeco CA6903 AquaClean',
+    newPrice: '44,20zł',
+    oldPrice: '52,00zł',
+  },
+  {
+    img: productImg3,
+    title: '2x Filtr Seltino SBH-Ultra zamiennik do Ultra Clarity 644845',
+    newPrice: '110,50zł',
+    oldPrice: '130,00zł',
+  },
+  {
+    img: productImg4,
+    title: 'ZESTAW: 2x Odkamieniacz Seltino Entkalker 1000 ml + szczoteczka',
+    newPrice: '30,02zł',
+    oldPrice: '35,32zł',
+  },
+]
+
 const ProductPromo = () => {
-  return <div className='productPromo'>productPromo</div>
+  const isDesktop = useIsDesktopDevice()
+
+  return (
+    <div className='productPromo'>
+      <p>Nasze produkty</p>
+      <Swiper
+        slidesPerView={isDesktop ? 4 : 1}
+        spaceBetween={39}
+        navigation={true}
+        modules={[Navigation]}
+        className='productPromo__carousel'
+      >
+        {productList.map((item, index) => (
+          <SwiperSlide key={index} className='productPromo__carousel-item'>
+            <img className='carousel__img' src={item.img} alt='product' />
+            <p className='carousel__title'>{item.title}</p>
+            <p className='carousel__newPrice'>{item.newPrice}</p>
+            <p className='carousel__oldPrice'>{item.oldPrice}</p>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  )
 }
 
 export default ProductPromo
